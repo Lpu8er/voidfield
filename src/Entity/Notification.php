b14b4503b5fdc54c2276b80f4f63955ea11a9240
Lpu8er <lpu8er@gmail.com>
2020-05-30 13:30:31 +0200

    Fix notifications

--- src/Entity/Notification.php
+++ src/Entity/Notification.php
@@ -12,6 +12,10 @@ use Doctrine\ORM\Mapping as ORM;
  */
 class Notification {
     const NOTIFTYPE_GENERIC = 'generic';
+    const NOTIFTYPE_ERROR = 'ko';
+    const NOTIFTYPE_WARN = 'warn';
+    const NOTIFTYPE_INFO = 'info';
+    const NOTIFTYPE_OK = 'ok';
     
     /**
      * 
@@ -21,14 +25,85 @@ class Notification {
      * @ORM\GeneratedValue(strategy="AUTO")
      */
     protected $id;
-    protected $dateNotify;
-    protected $dateRead;
+    /**
+     *
+     * @var \DateTime
+     * @ORM\Column(type="datetime", nullable=true)
+     */
+    protected $dateNotify = null;
+    /**
+     *
+     * @var \DateTime
+     * @ORM\Column(type="datetime", nullable=true)
+     */
+    protected $dateRead = null;
+    /**
+     *
+     * @var string 
+     * @ORM\Column(type="text")
+     */
     protected $content;
+    /**
+     *
+     * @var string 
+     * @ORM\Column(type="string", length=20)
+     */
     protected $notifType;
+    /**
+     *
+     * @var User
+     * @ORM\ManyToOne(targetEntity="User")
+     * @ORM\JoinColumn(referencedColumnName="id", nullable=true)
+     */
     protected $targetPlayer;
 
     public function getId(): ?int
     {
         return $this->id;
+    }
+    
+    public function getDateNotify(): \DateTime {
+        return $this->dateNotify;
+    }
+
+    public function getDateRead(): \DateTime {
+        return $this->dateRead;
+    }
+
+    public function getContent(): string {
+        return $this->content;
+    }
+
+    public function getNotifType(): string {
+        return $this->notifType;
+    }
+
+    public function getTargetPlayer(): User {
+        return $this->targetPlayer;
+    }
+
+    public function setDateNotify(\DateTime $dateNotify) {
+        $this->dateNotify = $dateNotify;
+        return $this;
+    }
+
+    public function setDateRead(\DateTime $dateRead) {
+        $this->dateRead = $dateRead;
+        return $this;
+    }
+
+    public function setContent(string $content) {
+        $this->content = $content;
+        return $this;
+    }
+
+    public function setNotifType(string $notifType) {
+        $this->notifType = $notifType;
+        return $this;
+    }
+
+    public function setTargetPlayer(User $targetPlayer) {
+        $this->targetPlayer = $targetPlayer;
+        return $this;
     }
 }
